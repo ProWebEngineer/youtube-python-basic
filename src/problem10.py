d6cfb1c9aae424a7b33862f6d5031b28d665d23b
Tsuyoshi Tomozawa <42736595+TsuyoshiTomozawa@users.noreply.github.com>
2025-09-18 07:36:55 +0900

    問題9-12

---
+++ src/problem10.py
@@ -0,0 +1,9 @@
+def main():
+    total = 0
+    for i in range(1, 11):
+        total += i
+    print(f"合計は {total} です")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/problem11.py
@@ -0,0 +1,7 @@
+def main():
+    fruits = ["りんご", "みかん", "ぶどう"]
+    for fruit in fruits:
+        print(f"{fruit}が好きです")
+
+if __name__ == "__main__":
+    main()
